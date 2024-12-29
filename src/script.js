027c283573c7175cef83a7e5f4adc15e6db0cdb8
shubhambendkhale77 <shubhambendkhale77@gmail.com>
2024-12-29 19:19:34 +0530

    Uploading project

---
+++ src/script.js
@@ -0,0 +1,82 @@
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+// Load movies from API
+async function loadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=42733cd6`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    if(data.Response === "True") displayMovieList(data.Search);
+}
+
+function findMovies(){
+    let searchTerm = movieSearchBox.value.trim();
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+function displayMovieList(movies){
+    searchList.innerHTML = "";
+    movies.forEach(movie => {
+        let moviePoster = movie.Poster !== "N/A" ? movie.Poster : "image_not_found.png";
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movie.imdbID; // setting movie id in data-id
+        movieListItem.classList.add('search-list-item');
+        movieListItem.innerHTML = `
+        <div class="search-item-thumbnail">
+            <img src="${moviePoster}" alt="Movie Poster">
+        </div>
+        <div class="search-item-info">
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+        </div>`;
+        searchList.appendChild(movieListItem);
+    });
+    loadMovieDetails();
+}
+
+function loadMovieDetails(){
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=42733cd6`);
+            const movieDetails = await result.json();
+            displayMovieDetails(movieDetails);
+        });
+    });
+}
+
+function displayMovieDetails(details){
+    let moviePoster = details.Poster !== "N/A" ? details.Poster : "image_not_found.png";
+    resultGrid.innerHTML = `
+    <div class="movie-poster">
+        <img src="${moviePoster}" alt="Movie Poster">
+    </div>
+    <div class="movie-info">
+        <h3 class="movie-title">${details.Title}</h3>
+        <ul class="movie-misc-info">
+            <li class="year">Year: ${details.Year}</li>
+            <li class="rated">Ratings: ${details.Rated}</li>
+            <li class="released">Released: ${details.Released}</li>
+        </ul>
+        <p class="genre"><b>Genre:</b> ${details.Genre}</p>
+        <p class="writer"><b>Writer:</b> ${details.Writer}</p>
+        <p class="actors"><b>Actors:</b> ${details.Actors}</p>
+        <p class="plot"><b>Plot:</b> ${details.Plot}</p>
+        <p class="language"><b>Language:</b> ${details.Language}</p>
+        <p class="awards"><b><i class="fas fa-award"></i></b> ${details.Awards}</p>
+    </div>`;
+}
+
+window.addEventListener('click', (event) => {
+    if(event.target.className !== "form-control"){
+        searchList.classList.add('hide-search-list');
+    }
+});
